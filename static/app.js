68af2eb33e3eb8f72238b0bc6b303f3cf425bf84
sarahtan931 <sarah.tan931@gmail.com>
2020-11-21 20:25:34 -0500

    more functionality

--- static/app.js
+++ static/app.js
@@ -18,6 +18,34 @@ fetch(url, {
   })
 }
 
+function redeem(){
+  tokens = document.getElementById('tokens').value;
+
+  const bodyredeem = {
+      "name": "TV",
+      "cost": 3
+  }
+
+  let url = "./api/chaching/tokens/Anusha/Sarah";
+
+  fetch(url, {
+  method: 'POST',
+  body: bodyredeem
+  })
+  .then(res => {
+    console.log('Added')
+    document.getElementById('tokens').innerHTML = 12;
+  })
+  .catch((error) =>{
+  console.log(error)
+
+})
+}
+
+function lock(){
+  document.getElementById('tokens').innerHTML = 8;
+  document.getElementById('locked').innerHTML = 11;
+}
 //function to create a schedule
 function deleteChild(){
     name = document.getElementById('adduser').value;
@@ -37,3 +65,30 @@ fetch(url, {
 
   })
 }
+
+function Toggle2() {
+    var x = document.getElementById("task2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function Toggle1() {
+    var x = document.getElementById("task1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function Toggle() {
+    var x = document.getElementById("task");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
